a1ac999bdb7f0cca7570b83630a8bf5e88d23701
zhouchaoran <zhouchaoran@threathunter.cn>
2023-11-11 17:11:34 +0800

    init commit

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,5 +1,5 @@
 hunter_config(CURL VERSION ${HUNTER_CURL_VERSION} CMAKE_ARGS HTTP_ONLY=ON CMAKE_USE_OPENSSL=OFF CMAKE_USE_LIBSSH2=OFF CURL_CA_PATH=none)
-hunter_config(Boost VERSION 1.70.0-p0)
+hunter_config(Boost VERSION 1.77.0)
 
 hunter_config(OpenCL VERSION
     URL https://github.com/KhronosGroup/OpenCL-SDK/releases/download/v2023.04.17/OpenCL-SDK-v2023.04.17-Source.tar.gz
